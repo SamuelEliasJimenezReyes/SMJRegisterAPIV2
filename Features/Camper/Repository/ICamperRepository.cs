492b7e8596fe5697bbc9f9c31342427c5497aeca
Samuel Jimenez <115100001+SamuelEliasJimenezReyes@users.noreply.github.com>
2025-08-25 00:37:42 -0400

    SJ-Add: Agregando Filtro en interface

--- Features/Camper/Repository/ICamperRepository.cs
+++ Features/Camper/Repository/ICamperRepository.cs
@@ -6,4 +6,5 @@ public interface ICamperRepository : IGenericRepository<Entities.Camper>
 {
     Task<List<Entities.Camper>> GetAllByChurchIDAsync(int churchID);
     Task<bool> ExistByPhoneNumber(string phoneNumber);
+    Task<List<Entities.Camper>> FindByFilterAsync(string? filter);
 }
