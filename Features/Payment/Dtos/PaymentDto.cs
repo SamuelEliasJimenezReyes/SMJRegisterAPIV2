7d46d7dd26b4d732aa400435e59f8f2e97c33772
Samuel Jimenez <115100001+SamuelEliasJimenezReyes@users.noreply.github.com>
2025-08-28 21:21:32 -0400

    SJ-Fix: Agregando Props a PaymentDto

--- Features/Payment/Dtos/PaymentDto.cs
+++ Features/Payment/Dtos/PaymentDto.cs
@@ -5,10 +5,12 @@ namespace SMJRegisterAPIV2.Features.Payment.Dtos;
 
 public class PaymentDto
 {
+    public int Id { get; set; }
     public decimal Amount { get; set; }
     public string? EvidenceURL { get; set; }
     public string? Coments { get; set; }
     public bool IsCash { get; set; }
+    public DateTime CreatedAt { get; set; }
 
     public BankInformationDto? BanksInformation { get; set; }
     
