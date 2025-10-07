28b07afe8ed77ef93433e6cf0c471f8c3f369d6d
Samuel Jimenez <115100001+SamuelEliasJimenezReyes@users.noreply.github.com>
2025-10-07 04:24:03 -0400

    SJ-Fix: Cambiando GetCamperByIdQueryHandler.cs

--- Features/Camper/Queries/GetById/GetCamperByIdQueryHandler.cs
+++ Features/Camper/Queries/GetById/GetCamperByIdQueryHandler.cs
@@ -1,6 +1,4 @@
-﻿using System.Globalization;
-using AutoMapper;
-using Humanizer;
+﻿using AutoMapper;
 using MediatR;
 using SMJRegisterAPIV2.Features.Camper.Dtos;
 using SMJRegisterAPIV2.Features.Camper.Repository;
@@ -9,31 +7,30 @@ using SMJRegisterAPIV2.Services.FileStore;
 namespace SMJRegisterAPIV2.Features.Camper.Queries.GetById;
 
 public class GetCamperByIdQueryHandler(
-    ICamperRepository repository, 
+    ICamperRepository repository,
     IMapper mapper,
     IFileStorage fileStorage)
     : IRequestHandler<GetCamperByIdQuery, CamperDTO>
 {
-
     public async Task<CamperDTO> Handle(GetCamperByIdQuery request, CancellationToken cancellationToken)
     {
         var entityDb = await repository.GetByIdAsync(request.ID);
         var mapped = mapper.Map<CamperDTO>(entityDb);
 
-        // if (!string.IsNullOrEmpty(mapped.DocumentsURL))
-        // {
-        //     try
-        //     {
-        //         var key = fileStorage.ExtractKeyFromUrl(mapped.DocumentsURL);
-        //         var signedUrl = fileStorage.GetSignedUrl(key, 60);
-        //         mapped.DocumentsURL = signedUrl;
-        //     }
-        //     catch (Exception ex)
-        //     {
-        //         Console.WriteLine($"Error generating signed URL: {ex.Message}");
-        //         mapped.DocumentsURL = null;
-        //     }
-        // }
+        if (!string.IsNullOrEmpty(mapped.DocumentsURL))
+        {
+            try
+            {
+                var key = fileStorage.ExtractKeyFromUrl(mapped.DocumentsURL);
+                var signedUrl = fileStorage.GetSignedUrl(key, 5);
+                mapped.DocumentsURL = signedUrl;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error generando URL firmada: {ex.Message}");
+                mapped.DocumentsURL = null;
+            }
+        }
 
         return mapped;
     }
